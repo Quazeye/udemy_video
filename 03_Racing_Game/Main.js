234690a66d8934cd2200328cd4d1c7519162a24a
De Neve <cmdeneve@w430-2446.local>
2017-03-03 15:11:55 -0700

    Split off graphics functions to GraphicsCommon.js

--- 03_Racing_Game/Main.js
+++ 03_Racing_Game/Main.js
@@ -241,34 +241,3 @@ function drawAll() {
 	drawTracks();
 	
 }
-
-// function to draw a bitmap with a rotation
-function drawBitmapCenteredWithRotation(useBitmap, atX, atY, withAng) {
-	
-	canvasContext.save();
-	canvasContext.translate(atX, atY);
-	canvasContext.rotate(withAng);
-	canvasContext.drawImage(useBitmap,-useBitmap.width/2, -useBitmap.height/2);
-	canvasContext.restore();
-
-}
-
-// function to draw colored rectangles
-function colorRect(topLeftX, topLeftY, boxWidth, boxHeight, fillColor) {
-	canvasContext.fillStyle = fillColor;
-	canvasContext.fillRect(topLeftX, topLeftY, boxWidth, boxHeight);
-}
-
-// function to draw colored circles
-function colorCircle(centerX, centerY, radius, fillColor) {
-	canvasContext.fillStyle = fillColor;
-	canvasContext.beginPath();
-	canvasContext.arc(centerX,centerY, radius, 0,Math.PI*2, true);
-	canvasContext.fill();
-}
-
-// function to draw colored text
-function colorText(showWords, textX, textY, fillColor) {
-	canvasContext.fillStyle = fillColor;
-	canvasContext.fillText(showWords, textX, textY);
-}
