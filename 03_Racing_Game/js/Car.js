fcd3adfc611db93634f647eb66a120dde223e610
Quazeye <quazeye@shaw.ca>
2017-03-08 21:41:16 -0700

    Create a carClass Class.

--- 03_Racing_Game/js/Car.js
+++ 03_Racing_Game/js/Car.js
@@ -1,9 +1,3 @@
-// car variables
-var carX = 75;
-var carY = 75;
-var carAng = 0;
-var carSpeed = 0;
-
 const GROUNDSPEED_DECAY_MULT = 0.94;
 const DRIVE_POWER = 0.5;
 const REVERSE_POWER = 0.2;
@@ -11,42 +5,49 @@ const TURN_RATE = 0.06;
 const MIN_SPEED_TO_TURN = 0.5;
 
 
+function carClass() {
 
-function carReset() {
-	for(var eachRow=0; eachRow<Track_Rows; eachRow++) {
-		for(var eachColumn=0; eachColumn<Track_Cols; eachColumn++) {
-			var arrayIndex = rowColToArrayIndex(eachColumn, eachRow);
-			if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
-				trackGrid[arrayIndex] = TRACK_ROAD;
-				carAng = -Math.PI/2;  // starts with car facing north
-				carX = eachColumn * Track_W + Track_W / 2;
-				carY = eachRow * Track_H + Track_H / 2;
+	this.x = 75;
+	this.y = 75;
+	this.ang = 0;
+	this.speed = 0;
+
+	this.reset = function() {
+		for(var eachRow=0; eachRow<Track_Rows; eachRow++) {
+			for(var eachColumn=0; eachColumn<Track_Cols; eachColumn++) {
+				var arrayIndex = rowColToArrayIndex(eachColumn, eachRow);
+				if(trackGrid[arrayIndex] == TRACK_PLAYERSTART) {
+					trackGrid[arrayIndex] = TRACK_ROAD;
+					carAng = -Math.PI/2;  // starts with car facing north
+					carX = eachColumn * Track_W + Track_W / 2;
+					carY = eachRow * Track_H + Track_H / 2;
+				} // end of player start if
+			} // end of col for
+		} // end of row for
+	} // end of carReset func
+
+	this.move = function() {
+		carSpeed *= GROUNDSPEED_DECAY_MULT;
+
+		if(keyHeld_Gas) {
+			carSpeed += DRIVE_POWER;
+		}
+		if(keyHeld_Reverse) {
+			carSpeed -= REVERSE_POWER;
+		}
+		if(Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
+			if(keyHeld_TurnLeft) {
+				carAng -= TURN_RATE;
+			}
+			if(keyHeld_TurnRight) {
+				carAng += TURN_RATE;
 			}
 		}
+		carX += Math.cos(carAng) * carSpeed;
+		carY += Math.sin(carAng) * carSpeed;
 	}
-}
 
-function carMove() {
-	carSpeed *= GROUNDSPEED_DECAY_MULT;
-
-	if(keyHeld_Gas) {
-		carSpeed += DRIVE_POWER;
-	}
-	if(keyHeld_Reverse) {
-		carSpeed -= REVERSE_POWER;
-	}
-	if(Math.abs(carSpeed) > MIN_SPEED_TO_TURN) {
-		if(keyHeld_TurnLeft) {
-			carAng -= TURN_RATE;
-		}
-		if(keyHeld_TurnRight) {
-			carAng += TURN_RATE;
-		}
-	}
-	carX += Math.cos(carAng) * carSpeed;
-	carY += Math.sin(carAng) * carSpeed;
-}
-
-function carDraw() {
+	this.draw = function() {
 		drawBitmapCenteredWithRotation(carPic, carX, carY, carAng);	
+	}
 }
